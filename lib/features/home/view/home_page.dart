9754453294642b8bc8c20bf8f5f4791f8fac9e1b
Yusup Narkuvatov <ynn.131095@gmail.com>
2025-12-16 01:07:16 +0300

    added geolocator and google_maps_flutter and geocoding

--- lib/features/home/view/home_page.dart
+++ lib/features/home/view/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stork/utils/constants/app_colors.dart';
+import 'package:stork/utils/constants/app_sizes.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
@@ -17,8 +19,81 @@ class _HomePageState extends ConsumerState<HomePage> {
         title: Text('Stork'),
         actions: [Icon(Icons.shopping_bag_outlined)],
       ),
-      body: Center(
-        child: Container(width: 200, height: 200, decoration: BoxDecoration(color: Colors.white)),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: PageView(
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(color: Colors.red),
+                  child: Center(child: Text('AA')),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(color: Colors.purple),
+                  child: Center(child: Text('AA')),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(color: Colors.green),
+                  child: Center(child: Text('BB')),
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(color: Colors.red),
+                  child: Center(child: Text('CC')),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 6,
+            child: Row(
+              children: [
+                Card(
+                  color: AppColors.primaryBackground,
+                  // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                  child: Ink(
+                    height: 200,
+                    width: 150,
+                    // padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/categories/atistirmalik.webp'),
+                        alignment: Alignment.bottomLeft,
+                      ),
+                    ),
+                    child: InkWell(
+                      onTap: () {},
+                      // splashColor: Colors.red,
+                      // borderRadius: BorderRadius.circular(12),
+                      customBorder: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                      // splashFactory: ,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: AppSizes.md,
+                          vertical: AppSizes.sm,
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [Text('ada'), Text('aaaa')],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Container(width: 200, height: 200, decoration: BoxDecoration(color: Colors.white)),
+        ],
       ),
     );
   }
--- lib/utils/constants/app_image_strings.dart
+++ lib/utils/constants/app_image_strings.dart
@@ -4,6 +4,9 @@ class AppImageString {
   // App Logos
   static const String lightAppLogo = "assets/logos/logo-black.png";
   static const String darkAppLogo = "assets/logos/logo-black.png";
+  // Categories Images
+  static const String atistirmalik = "assets/images/categories/atistirmalik.webp";
+  static const String bebek = "assets/images/categories/bebek.webp";
 
   // Social Logos
   static const String google = "assets/logos/google-icon.png";
