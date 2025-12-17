cc3260fea52f4fc370f599e1d9fd1c332b5eb316
Yusup Narkuvatov <ynn.131095@gmail.com>
2025-12-17 15:44:05 +0300

    new bottomnavigationbar

--- lib/features/Apps/Stork_Shop/stork_shop.dart
+++ lib/features/Apps/Stork_Shop/stork_shop.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -14,178 +15,136 @@ class StorkShop extends ConsumerStatefulWidget {
 }
 
 class _StorkShopState extends ConsumerState<StorkShop> {
-  int count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        foregroundColor: Colors.white,
-        title: Text('Stork'),
-        actions: [Icon(Icons.shopping_bag)],
-      ),
+      appBar: _appBar(),
       body: widget.navigationShell,
-
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) {
-          widget.navigationShell.goBranch(index);
-        },
-        currentIndex: widget.navigationShell.currentIndex,
-        type: BottomNavigationBarType.fixed,
-        selectedIconTheme: IconThemeData(color: AppColors.primary),
-        unselectedIconTheme: IconThemeData(color: AppColors.textSecondary),
-        selectedItemColor: AppColors.primary,
-        unselectedItemColor: AppColors.textSecondary,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedFontSize: 12.0,
-        unselectedFontSize: 12.0,
-        backgroundColor: AppColors.surface,
-        selectedLabelStyle: AppTextStyles.bottonNavigationBarLabel,
-        iconSize: AppSizes.xl,
-
-        // backgroundColor: Colors.amber,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: 'Card'),
-
-          BottomNavigationBarItem(icon: Icon(Icons.receipt_long), label: 'Orders'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
+      bottomNavigationBar: BottomAppBar(
+        color: AppColors.surface,
+        padding: EdgeInsets.zero,
+        height: 60,
+        elevation: 10,
+        shadowColor: Colors.red,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildInkWellItem(Icons.home, "Home", 0, navigationShell: widget.navigationShell),
+            _buildInkWellItem(Icons.search, "Search", 1, navigationShell: widget.navigationShell),
+            _buildInkWellItem(
+              Icons.shopping_bag,
+              "Card",
+              2,
+              navigationShell: widget.navigationShell,
+            ), // Orta buton artık normal
+            _buildInkWellItem(
+              Icons.receipt_long,
+              "Orders",
+              3,
+              navigationShell: widget.navigationShell,
+            ),
+            _buildInkWellItem(Icons.person, "Profile", 4, navigationShell: widget.navigationShell),
+          ],
+        ),
       ),
-    );
-  }
-
-  NavigationBar navi(BuildContext context) {
-    return NavigationBar(
-      onDestinationSelected: widget.navigationShell.goBranch,
-      selectedIndex: widget.navigationShell.currentIndex,
-      backgroundColor: AppColors.surface,
-      indicatorColor: Colors.transparent,
-      // height: 76,
-      maintainBottomViewPadding: false,
-      surfaceTintColor: AppColors.surface,
-      shadowColor: Colors.amberAccent,
-      indicatorShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
-      labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
-      labelTextStyle: WidgetStateTextStyle.resolveWith((states) {
-        if (states.contains(WidgetState.selected)) {
-          return TextStyle(color: AppColors.primary);
-        }
-        return TextStyle(color: AppColors.textSecondary);
-      }),
-      destinations: [
-        navigationBarMenu(
-          context,
-          index: 0,
-          currentIndex: widget.navigationShell.currentIndex,
-          label: "Home",
-          icon: Icons.home,
-        ),
-        navigationBarMenu(
-          context,
-          index: 1,
-          currentIndex: widget.navigationShell.currentIndex,
-          label: "Search",
-          icon: Icons.search,
-        ),
-        navigationBarMenu(
-          context,
-          index: 2,
-          currentIndex: widget.navigationShell.currentIndex,
-          label: "Card",
-          icon: Icons.shopping_bag,
-        ),
-        navigationBarMenu(
-          context,
-          index: 3,
-          currentIndex: widget.navigationShell.currentIndex,
-          label: "Orders",
-          icon: Icons.receipt_long,
-        ),
-        navigationBarMenu(
-          context,
-          index: 4,
-          currentIndex: widget.navigationShell.currentIndex,
-          label: "Profile",
-          icon: Icons.person,
-        ),
-      ],
     );
   }
 }
 
-Widget navigationBarMenu(
-  BuildContext context, {
-  required int index,
-  required int currentIndex,
-  required String label,
-  required IconData icon,
+Widget _buildInkWellItem(
+  IconData icon,
+  String label,
+  int index, {
+  required StatefulNavigationShell navigationShell,
 }) {
-  return NavigationDestination(
-    icon: Padding(
-      padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 22),
-      child: Icon(
-        icon,
-        size: 32,
-        color: currentIndex == index ? AppColors.primary : AppColors.textSecondary,
+  bool isSelected = navigationShell.currentIndex == index;
+
+  return Expanded(
+    child: InkResponse(
+      radius: 50,
+      // borderRadius: BorderRadius.circular(50),
+      // customBorder: StadiumBorder(),
+      onTap: () {
+        navigationShell.goBranch(index);
+      },
+      // Dalga efekti rengi
+      highlightColor: AppColors.primary.withAlpha(20),
+      splashColor: AppColors.primary.withAlpha(50),
+      splashFactory: InkRipple.splashFactory,
+      // customBorder: StadiumBorder(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: isSelected ? AppColors.primary : AppColors.textSecondary,
+            size: AppSizes.iconXl,
+          ),
+          // const SizedBox(height: 4),
+          Text(
+            label,
+            style: AppTextStyles.withColor(
+              AppTextStyles.bottonNavigationBarLabel,
+              isSelected ? AppColors.primary : AppColors.textSecondary,
+            ),
+          ),
+        ],
       ),
     ),
-    label: label,
   );
 }
+
+// TextStyle(
+//               fontSize: AppSizes.iconXs,
+//               color: isSelected ? AppColors.primary : AppColors.textSecondary,
+//               fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+//             ),
+//* Widgets
+
+// App Bar Widget
+AppBar _appBar() {
+  return AppBar(
+    foregroundColor: Colors.white,
+    title: Text('Stork'),
+    actions: [Icon(Icons.shopping_bag)],
+  );
+}
+
+// BottomNavigationBar Widget
+Widget bottomNavigationBar(StatefulNavigationShell navigationShell) {
+  return BottomNavigationBar(
+    onTap: (index) {
+      navigationShell.goBranch(index);
+    },
+    currentIndex: navigationShell.currentIndex,
+    type: BottomNavigationBarType.fixed,
+    selectedIconTheme: IconThemeData(color: AppColors.primary),
+    unselectedIconTheme: IconThemeData(color: AppColors.textSecondary),
+    selectedItemColor: AppColors.primary,
+    unselectedItemColor: AppColors.textSecondary,
+    showSelectedLabels: true,
+    showUnselectedLabels: true,
+    selectedFontSize: 12.0,
+    unselectedFontSize: 12.0,
+    backgroundColor: AppColors.surface,
+    selectedLabelStyle: AppTextStyles.bottonNavigationBarLabel,
+    iconSize: AppSizes.xl,
+
+    // backgroundColor: Colors.amber,
+    items: [
+      BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+      BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+
+      BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: 'Card'),
+
+      BottomNavigationBarItem(icon: Icon(Icons.receipt_long), label: 'Orders'),
+      BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+    ],
+  );
+}
+
+
 /*
-
-BottomNavigationBar(
-        selectedItemColor: AppColors.primary,
-        unselectedItemColor: AppColors.textSecondary,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedFontSize: 12.0,
-        unselectedFontSize: 12.0,
-        iconSize: 26,
-        elevation: 1,
-        onTap: (index) {},
-        type: BottomNavigationBarType.fixed,
-        // backgroundColor: Colors.amber,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: AppColors.surface,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search, color: AppColors.textSecondary),
-            label: 'Search',
-            backgroundColor: AppColors.surface,
-          ),
-          BottomNavigationBarItem(
-            icon: Opacity(opacity: 0, child: Icon(Icons.no_cell)),
-            label: '', // Yazı da boş kalsın ki butonun altına girmesin
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag, color: AppColors.textSecondary),
-            label: 'Card',
-            backgroundColor: AppColors.surface,
-          ),
-
-          // BottomNavigationBarItem(
-          //   icon: Icon(Icons.receipt_long, color: AppColors.textSecondary),
-          //   label: 'Orders',
-          //   backgroundColor: AppColors.surface,
-          // ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person, color: AppColors.textSecondary),
-            label: 'Profile',
-            backgroundColor: AppColors.surface,
-          ),
-        ],
-      ),
-
-
-
-
 
 Getir’de KONUM AKIŞI (gerçek mimari)
 Kullanıcı uygulamayı açar
